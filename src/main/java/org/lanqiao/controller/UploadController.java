ed0a73407ce90163e36b99d120fd6a82a1e93192
gosick39 <1789748171@qq.com>
2019-09-18 19:39:30 +0800

    增加上传删除稿件功能 9/18

--- src/main/java/org/lanqiao/controller/UploadController.java
+++ src/main/java/org/lanqiao/controller/UploadController.java
@@ -148,10 +148,18 @@ public class UploadController {
     }
 
     @RequestMapping(value = "/managerVideo")
-    public ModelAndView managerVideo(@RequestParam(value="pageNo", defaultValue = "1") int pageNum, @RequestParam(value="isReview", defaultValue = "-1") int isReview, @RequestParam(value="videoId", defaultValue = "-1") int videoId) {
+    public ModelAndView managerVideo(@RequestParam(value="pageNo", defaultValue = "1") int pageNum, @RequestParam(value="isReview", defaultValue = "-1") int isReview, @RequestParam(value="videoId", defaultValue = "-1") final int videoId) {
         ModelAndView modelAndView = new ModelAndView();
         if(videoId!=-1){
-            uploadService.delVideo(videoId);
+            new Thread() {
+                public void run() {
+                    Ffmpeg ffmpeg = new Ffmpeg();
+                    Video video = videoService.selectVideoInfo(videoId);
+                    ffmpeg.delDataFile(video.getVideoPic()); //删除该稿件的封面
+                    ffmpeg.delDataFile(video.getVideoUrl());  //删除该稿件的视频文件
+                }
+            }.start();
+            uploadService.delVideo(videoId); //删除该稿件的数据库记录
         }
         if(isReview==-1){
             PageHelper.startPage(pageNum, 5);
